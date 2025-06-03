a720e234f1473aa3398d026331023bb15e2f56e2
Masatoshi Ito <botesan1216@gmail.com>
2025-06-03 12:47:05 +0900

    ソースのUTF-8化対応（verbose出力以外）や各種テンプレート対応など
    ・Gitでバージョン管理するようにしたので差分コメント削除
    ・タブのスペース化
    ・テンプレート対応
    　　jachars,zhchars,jachar,zhchar,部首,総画（完全ではない）
    　　contextの一部

--- wiktionary/wikipedia-fpw/entities.pl
+++ wiktionary/wikipedia-fpw/entities.pl
@@ -1,8 +1,11 @@
+require v5.6;
+use utf8;
 use strict;
 use warnings;
 
 use English;
 use FileHandle;
+use Encode qw/decode_utf8 encode_utf8/;
 
 #
 # ダンプデータ中に使用されている実体参照を出力します。
@@ -25,6 +28,7 @@ use FileHandle;
      if (!defined($_)) {
        last;
      }
+     $_ = decode_utf8($_);
 
      while ($_ =~ s/&amp;([0-9a-zA-Z]+);//){
        $entities{$1} = 1;
@@ -32,6 +36,6 @@ use FileHandle;
    }
 
    foreach $entity (keys(%entities)) {
-     print "'$entity' => '?',\n";
+     print encode_utf8("'$entity' => '?',\n");
    }
 }
