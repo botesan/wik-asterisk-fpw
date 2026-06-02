f07828d34f25eaf9c4cd92d1854f6b9a2161ac3e
Masatoshi Ito <botesan1216@gmail.com>
2026-06-02 23:14:21 +0900

    テンプレートの対応
    ・ja-headword系の修正
    ・kangokana関係の追加
    ・ja-homonym,ja-hom関係の追加
    ・ja-headword系の修正（ja-adjの追加）
    ・分綴系 hyphenation, hyph の追加
    ・contextの対応を一部追加

--- wiktionary/template.bash
+++ wiktionary/template.bash
@@ -7,3 +7,8 @@ grep -P '{@.*@}' log.txt | \
 sort < notreplacetemplate.txt | \
     uniq -c | \
     sort > uniq-notreplacetemplate.txt
+
+grep -P '{@context@' log.txt | \
+    sed -E 's/^[^{]*\{@([^:@}]+)@//;s/@}.*/|/;s/lang=[a-zA-Z]+//;s/\|\|/|/;s/^\|//;s/\|$//;s/\|/\t/g;' | \
+    sort | uniq -c | \
+    sort > uniq-notreplacecontext.txt
